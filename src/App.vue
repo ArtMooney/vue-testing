30207402c41de841cfd6c24be196d106b0c4329f
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2020-08-27 09:13:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <Dick/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Dick from "./components/Dick";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Dick
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Dick.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>KUKEN</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dick"
+};
+</script>
